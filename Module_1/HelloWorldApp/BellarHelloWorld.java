1f941d7f0f2c05acfcf222d91e1fc7492c40f2e9
Chris <cmbellar@my365.bellevue.edu>
2023-09-03 22:45:15 -0400

    Assignment 1.2

---
+++ Module_1/HelloWorldApp/BellarHelloWorld.java
@@ -0,0 +1,22 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by C. Bellar 2023
+*/ 
+public class BellarHelloWorld {
+
+    /**
+     * Method with two arguments, which are used to print Hello World along with the user's first and last name.
+     * @param firstName String 
+     * @param lastName String 
+     * @return prints the results to the console window.
+     */
+    public static void sayHello(String firstName, String lastName) {
+        System.out.printf("Hello World from %s %s", firstName, lastName);
+    } // end sayHello
+
+    public static void main(String[] args) {
+        sayHello("Chris", "Bellar"); // Call the sayHello method and pass "Chris" and "Bellar" as values.
+    } // end main
+} // end BellarHelloWorld
